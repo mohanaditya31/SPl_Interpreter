66e5cab28009d6b83ee1969a426b69e920b078d1
mohanaditya31 <mohanaditya523@gmail.com>
2026-04-24 17:39:56 +0530

    User defined function and caller added

--- Interpreter.py
+++ Interpreter.py
@@ -1,4 +1,5 @@
 import math
+import re
 INTEGER, PLUS, EOF, OPERATION, FUNCTION,CONSTANT = 'INTEGER', 'PLUS', 'EOF', 'OPERATION', 'FUNCTION', 'CONSTANT'
 
 
@@ -57,7 +58,7 @@ class Condition:
 
 class Interpreter:
 
-    def __init__(self,text,variable = None):
+    def __init__(self,text,variable = None,made_functions=None):
 
         self.text =text 
         self.pos =0
@@ -69,14 +70,17 @@ class Interpreter:
         self.parse_pos = 0
         self.variable = variable if variable is not None else {'a': 0, 'b': 0, 'c': 0}
         self.variable_write = ["a=","b=","c="]
+        self.function = ["f","g"]
+        self.made_functions = made_functions if made_functions is not None else {}
 
 
 
-    def error(self):
+
+    def error(self):                                                    #Invalid Input
         print("Invalid Input")
         raise Exception('Error parsing input')
     
-    def finalize_token(self):
+    def finalize_token(self):                                           # Used to make tokens and store it 
         if self.str == "":
             return
 
@@ -100,6 +104,10 @@ class Interpreter:
 
         elif self.str == "if":
             self.tokens.append(Token(self.str,"if_then"))
+
+        elif self.str in self.function:
+            self.tokens.append(Token(self.str,"Function_call"))
+
         
 
         else:
@@ -132,7 +140,45 @@ class Interpreter:
         
         return self.parse()
 
-    def parse(self):
+# -------------------------------------
+
+
+    def capture_one(self):
+            """Capture tokens for one complete expression without evaluating."""
+            start = self.parse_pos
+            self._skip_expr()
+            return self.tokens[start:self.parse_pos]
+
+    def _skip_expr(self):
+        """Advance parse_pos past one complete expression without evaluating."""
+        if self.parse_pos >= len(self.tokens):
+            return
+        token = self.tokens[self.parse_pos]
+        self.parse_pos += 1
+
+        if token.ty in (INTEGER, CONSTANT, "Variable_read"):
+            return                          # single token, done
+        elif token.ty == OPERATION:
+            self._skip_expr()               # skip left
+            self._skip_expr()               # skip right
+        elif token.ty == FUNCTION:
+            self._skip_expr()               # skip one argument
+        elif token.ty == "if_then":
+            self._skip_expr()               # skip condition
+            self._skip_expr()               # skip true branch
+            self._skip_expr()               # skip false branch
+        elif token.ty in ("Variable_write", "Function_call"):
+            self._skip_expr()               # skip one argument
+
+    def tokens_to_string(self, tokens):
+        """Convert a token list back to a space-separated string."""
+        return " ".join(str(t.val) for t in tokens)
+
+
+
+#------------------------------------
+
+    def parse(self):                                                        #How tokens will work
         current_token = self.tokens[self.parse_pos]
         self.parse_pos +=1
 
@@ -171,19 +217,46 @@ class Interpreter:
                 return self.variable["b"]
             elif current_token.val == 'c':
                 return self.variable["c"]
+            elif current_token.val == 'x':
+                return self.variable["x"]
             
         elif current_token.ty == "if_then":
-            condit = self.parse()
-            out1 = self.parse()
-            out2 = self.parse()
-            return Condition.eval(condit,out1,out2)
+            condit = self.parse()                       # evaluate condition eagerly
+            true_tokens  = self.capture_one()           # capture, don't evaluate
+            false_tokens = self.capture_one()           # capture, don't evaluate
+
+            if condit != 0:
+                true_str = self.tokens_to_string(true_tokens)
+                temp = Interpreter(true_str, self.variable, self.made_functions)
+                return temp.expr()
+            else:
+                false_str = self.tokens_to_string(false_tokens)
+                temp = Interpreter(false_str, self.variable, self.made_functions)
+                return temp.expr()
+        
+        elif current_token.ty == "Function_call":
+            if current_token.val == "f":         
+                arg = self.parse()                  # step 2: evaluate argument first
+                self.variable['x'] = arg     
+                body = self.made_functions['f']
+                substituted = re.sub(r'\bx\b', str(arg), body)  # step 5: substitute
+                interpreter2 = Interpreter(substituted,self.variable,self.made_functions)
+                result = interpreter2.expr()
+                return result
+            elif current_token.val == "g":
+                self.variable['x'] = self.parse()
+                interpreter2 = Interpreter(self.made_functions['g'],self.variable,self.made_functions)
+                result = interpreter2.expr()
+                return result
 
         
         return 0
         
 
 def main():
-    variables = {'a':0 , 'b':0,'c':0}
+    variables = {'a':0 , 'b':0,'c':0,'x':0}
+    made_functions = {'f':0,'g':0}
+    
     while True:
         try:
             text = input('spl> ')
@@ -194,7 +267,16 @@ def main():
         if text == "exit":
             print("bye!")
             break
-        interpreter = Interpreter(text,variables)
+
+        if len(text) >= 2 and text[1] == '=' and (text[0] == 'f' or text[0] == 'g'):
+            fname = text[0]              # "f"
+            body  = text[2:].strip()     # "+ x 1"
+            made_functions[fname] = body
+            print(f"function {fname} defined")
+            
+            continue
+        
+        interpreter = Interpreter(text,variables,made_functions)
         result = interpreter.expr()
         print(result)
 
